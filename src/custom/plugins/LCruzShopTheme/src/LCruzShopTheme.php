8bebe11e637ea6b3fa616f7b79151bf22510e41b
luiquecruz <luiquecruz@gmail.com>
2022-05-10 15:22:35 -0300

    feat: Add LCruzShopTheme

---
+++ src/custom/plugins/LCruzShopTheme/src/LCruzShopTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace LCruzShopTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class LCruzShopTheme extends Plugin implements ThemeInterface
+{
+}
