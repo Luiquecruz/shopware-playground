2ff2c1d970a02812e1f8d7e456423c2854731931
luiquecruz <luiquecruz@gmail.com>
2022-05-24 15:48:20 -0300

    feat: Add Caravel X base theme

---
+++ src/custom/plugins/CaravelXBaseTheme/src/CaravelXBaseTheme.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace CaravelXBaseTheme;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class CaravelXBaseTheme extends Plugin implements ThemeInterface
+{
+}
